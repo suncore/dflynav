10d2844539b2ed8ffc51563a587a048da319e1ba
ervhch <ervhch@EV441EA1CE666D.eemea.ericsson.se>
2012-03-12 14:26:55 +0100

    e

--- src/Df_Dialog.py
+++ src/Df_Dialog.py
@@ -1,4 +1,11 @@
 
 
+from PySide.QtCore import *
+from PySide import QtGui
+import Df
 
-
+def Dialog(title, instruction, prefill):    
+    result, ok = QtGui.QInputDialog.getText(Df.d.g.mw, title, instruction, QtGui.QLineEdit.Normal, prefill)
+    if ok and result != '':
+        return result
+    return None
