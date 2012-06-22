c5b9ce9d6c1ee0809ceea22fd9cf3e9cc3858893
hch <henrik@harmsen.se>
2012-06-22 09:53:31 +0200

    k

--- src/Df_UiFind.py
+++ src/Df_UiFind.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'find.ui'
 #
-# Created: Fri Jun 22 09:50:17 2012
+# Created: Fri Jun 22 09:53:16 2012
 #      by: pyside-uic 0.2.13 running on PySide 1.1.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -20,16 +20,19 @@ class Ui_Find(object):
         self.gridLayout.addWidget(self.input, 1, 0, 1, 1)
         self.instruction = QtGui.QLabel(Find)
         self.instruction.setObjectName("instruction")
-        self.gridLayout.addWidget(self.instruction, 3, 0, 1, 1)
+        self.gridLayout.addWidget(self.instruction, 4, 0, 1, 1)
         self.hitlist = QtGui.QTreeWidget(Find)
         self.hitlist.setObjectName("hitlist")
-        self.gridLayout.addWidget(self.hitlist, 4, 0, 1, 1)
+        self.gridLayout.addWidget(self.hitlist, 6, 0, 1, 1)
         self.heading = QtGui.QLabel(Find)
         self.heading.setObjectName("heading")
         self.gridLayout.addWidget(self.heading, 0, 0, 1, 1)
         self.recursive = QtGui.QCheckBox(Find)
         self.recursive.setObjectName("recursive")
         self.gridLayout.addWidget(self.recursive, 2, 0, 1, 1)
+        self.status = QtGui.QLabel(Find)
+        self.status.setObjectName("status")
+        self.gridLayout.addWidget(self.status, 3, 0, 1, 1)
 
         self.retranslateUi(Find)
         QtCore.QMetaObject.connectSlotsByName(Find)
@@ -40,4 +43,5 @@ class Ui_Find(object):
         self.hitlist.headerItem().setText(0, QtGui.QApplication.translate("Find", "Object", None, QtGui.QApplication.UnicodeUTF8))
         self.heading.setText(QtGui.QApplication.translate("Find", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
         self.recursive.setText(QtGui.QApplication.translate("Find", "Search sub-folders recursively", None, QtGui.QApplication.UnicodeUTF8))
+        self.status.setText(QtGui.QApplication.translate("Find", "Result", None, QtGui.QApplication.UnicodeUTF8))
 
