984945291c55bd2518af65c08e490d4c15d4f9ad
hch <hch@4820tg>
2012-10-25 21:40:47 +0200

    xs

--- src/Df_UiMainwin.py
+++ src/Df_UiMainwin.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'mainwin.ui'
 #
-# Created: Mon Oct 22 11:52:22 2012
-#      by: pyside-uic 0.2.13 running on PySide 1.1.0
+# Created: Thu Oct 25 20:16:35 2012
+#      by: pyside-uic 0.2.14 running on PySide 1.1.1
 #
 # WARNING! All changes made in this file will be lost!
 
