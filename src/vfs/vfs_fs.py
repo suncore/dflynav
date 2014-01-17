1a8939b0a3ed7a36d643b1f5edfc194eff0a1c7a
hch <hch@server>
2014-01-17 14:33:50 +0100

    linux unpack fix 2

--- src/vfs/vfs_fs.py
+++ src/vfs/vfs_fs.py
@@ -633,7 +633,7 @@ class Cmd(Df_Job.Cmd):
             si.dwFlags = subprocess.STARTF_USESHOWWINDOW
             si.wShowWindow = subprocess.SW_HIDE
         try:
-            print cmd
+            #print cmd
             if workingDir:
                 self.pob = Popen(cmd, bufsize=1, stdout=PIPE, stderr=STDOUT, universal_newlines=True, cwd=workingDir, startupinfo=si)
             else:
